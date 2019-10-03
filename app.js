db7fc1d75cf5d4c7bc07d16be1dfe0d9d01bc2f7
Swampthing <52452391+swampcoin@users.noreply.github.com>
2019-10-03 06:01:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -22,9 +22,7 @@ const sass = require('node-sass-middleware');
 const multer = require('multer');
 const bitcoin = require('bitcoin');
 const WAValidator = require('wallet-address-validator');
-/*
 const recaptcha = require('express-recaptcha');
-*/
 const QRCode = require('qrcode');
 const base32 = require('thirty-two');
 const sprintf = require('sprintf');
@@ -83,9 +81,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-/* 
 recaptcha.init(process.env.RECAPTCHA_PUBLIC, process.env.RECAPTCHA_PRIVATE);
-*/
 app.use(session({
   resave: true,
   saveUninitialized: true,
@@ -130,12 +126,11 @@ app.get('/', homeController.index);
 app.get('/login', userController.getLogin);
 app.post('/login', function (req, res, next) {
         recaptcha.verify(req, function (error) {
-  /*          if (!error)
+            if (!error)
                 return next();
             else
                 req.flash('errors', { msg: 'Recaptcha is invalid!' });
-                return res.redirect('/login');
-*/          
+                return res.redirect('/login');        
         });
     }, userController.postLogin
 );
@@ -147,7 +142,7 @@ app.post('/forgot', userController.postForgot);
 app.get('/reset/:token', userController.getReset);
 app.post('/reset/:token', userController.postReset);
 app.get('/signup', userController.getSignup);
-app.post('/signup', function (req, res, next) /* {
+app.post('/signup', function (req, res, next)  {
          recaptcha.verify(req, function (error) {
           
             if (!error)
@@ -159,7 +154,7 @@ app.post('/signup', function (req, res, next) /* {
         });
         
     }, userController.postSignup
-);*/
+);
 app.get('/contact', contactController.getContact);
 app.post('/contact', contactController.postContact);
 app.get('/account', passportConfig.isAuthenticated, passportConfig.ensureTotp, userController.getAccount);
